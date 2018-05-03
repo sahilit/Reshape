46a4e4f53ba5c7934dde0ec21e3e3843822b7400
Sahil Singh <sahilsinghmca@gmail.com>
2018-05-03 17:30:37 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>BMI Calculator</title>
+
+		<?php include "include/css.php" ?>
+	</head>
+	<body>
+	
+		<?php include "include/navbar.php" ?>
+	
+	
+	
+	
+		<?php include "include/js.php" ?>
+		
+	</body>
+</html>
