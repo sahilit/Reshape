4bf1ae88c9305fe44af663953edef74d114f5831
Sahil Singh <sahilsinghmca@gmail.com>
2018-05-03 17:31:30 +0530

    Add files via upload

---
+++ include/css.php
@@ -0,0 +1,9 @@
+
+<!-- Main CSS -->
+<link rel="stylesheet" href="css/main.css" >
+		
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+		
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
