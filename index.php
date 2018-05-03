08aba4047522b2614e19cf915a8ce6974643db70
Sahil Singh <sahilsinghmca@gmail.com>
2018-05-03 19:19:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,45 @@
 	</head>
 	<body>
 		
-		<div id="aa">
-			<?php include "include/navbar.php" ?>
-	
+		<div id="first-page">	
+			<div class="top-header">
+				<div class="top-header-left">
+					<p><i class="fa fa-phone" aria-hidden="true"></i>+1 420-000-6000</p>
+					<p><i class="fa fa-envelope" aria-hidden="true"></i>hello@website.com</p>
+					<p><i class="fa fa-clock-o" aria-hidden="true"></i>6am - 11pm</p>
+				</div>
+				<div class="top-header-center">
+					<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+				</div>
+				<div class="top-header-right">
+					<a href="#"><i class="fa fa-search" aria-hidden="true"></i></a><input type="text" class="search">
+				</div>
+				<div class="clear">
+				</div>
+			</div>
+			<div class="bottom-header">
+				<div class="bottom-header-left">
+					<a href="#"><img src="images/logo.jpg"></a>
+				</div>
+				<div class="bottom-header-right">
+					<a href="#" class="nav-button"><i class="fa fa-bars" aria-hidden="true"> Menu</i></a>
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="about_us.php">About Us</a></li>
+						<li><a href="bmi_calculator.php">BMI Calculator</a></li>
+						<li><a href="gallery.php">Gallery</a></li>
+						<li><a href="events.php">Events</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+				<div class="clear">
+				</div>
+			</div>
+
 			<div class="slider-container">
 				<div class="slider-control left inactive"></div>
 				<div class="slider-control right"></div>
